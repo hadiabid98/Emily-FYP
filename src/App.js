231e24ec464548abea0dae69579c61da80b97e2b
Abdul Hadi <sp18-bcs-003@student.comsats.edu.pk>
2021-04-09 13:12:38 +0500

    app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="div">
       <Header/>
         <p>
-          Edit <code>src.js</code> and save to reload.
+          Edit <code>s rc.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
