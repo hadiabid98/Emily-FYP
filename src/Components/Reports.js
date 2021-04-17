f95a5abd416e14450a503421ab2b4ea5229fd920
Aleena-Naveed <aleenanaveed00@gmai.com>
2021-04-17 16:53:58 +0500

    Report

---
+++ src/Components/Reports.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import '../Stylesheet.css';
+import file from '../assets/file.png';
+import email from '../assets/email.png';
+import comment from '../assets/comment.png';
+import Header from './Header';
+function Report() {
+
+    const [getTimer, setTimer]=useState(30)
+    const [getEmailVerify, setEmailVerify] = useState("");
+    const [getConVerify, setConVerify] = useState("");
+
+    return (
+        <div className="div" >
+            <Header />
+            <div className="dialog_box_profile">
+                <span className="title">REPORTS</span>
+                <hr id='hr'></hr>
+                <p className="row" >
+                    <span className="label_li width5p">NO </span>
+                    <span className="label_li width40p"> REPORT DATE AND TIME </span>
+                    <span className="label_li width40p"> REPORT TITLE </span>
+                    <span className="label_li width15p"> ACTIONS </span>
+                </p>
+                <hr id='hr'></hr>
+                <div className="row" >
+                    <span className="label_li width5p">01 </span>
+                    <span className="label_li width40p"> FEB 21,2021 | 10:37:56 </span>
+                    <span className="label_li width40p"> PATIENT 312_SESSION_REPORT </span>
+                    <span className="label_li width15p">
+                        <img src={file} className="report_img"/> 
+                        <img src={email}className="report_img"/>
+                         <img src={comment} className="report_img"/>
+                    </span>
+                </div>
+                <div className="row" >
+                    <span className="label_li width5p">02 </span>
+                    <span className="label_li width40p"> FEB 21,2021 | 10:37:56 </span>
+                    <span className="label_li width40p"> PATIENT 312_SESSION_REPORT </span>
+                    <span className="label_li width15p">
+                        <img src={file} className="report_img"/> 
+                        <img src={email}className="report_img"/>
+                         <img src={comment} className="report_img"/>
+                    </span>
+                </div>
+                <div className="row" >
+                    <span className="label_li width5p">03 </span>
+                    <span className="label_li width40p"> FEB 21,2021 | 10:37:56 </span>
+                    <span className="label_li width40p"> PATIENT 312_SESSION_REPORT </span>
+                    <span className="label_li width15p">
+                        <img src={file} className="report_img"/> 
+                        <img src={email}className="report_img"/>
+                         <img src={comment} className="report_img"/>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Report;
