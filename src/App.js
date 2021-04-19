3229e574c7adfd158f64877b09fc8a5874e0a9d1
Abdul Hadi <sp18-bcs-003@student.comsats.edu.pk>
2021-04-20 00:09:52 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './Stylesheet.css'
 // import Profile from "./components/UserProfile"
 // import Signup from "./Components/Sign-up"
 // import Verify from "./Components/Verification"
-<<<<<<< HEAD
 // import Report from "./Components/Reports"
 import Settings from "./Components/UserSettings" 
 
@@ -12,14 +11,6 @@ function App() {
   return (
     <div className="div">
       <Settings />
-=======
-// import Dashboard from "./components/Dashboard"
-import REPORTS from './components/Reports'
-function App() {
-  return (
-    <div className="div">
-      <REPORTS/>
->>>>>>> 0f7f6b62789046a2d6d13a556bc33ea68a566f51
     </div>
   );
 }
