7a24aa9ff1c5a85588f0fbd8380ab8140a1bd7b6
Aleena-Naveed <aleenanaveed00@gmai.com>
2021-04-19 15:25:48 +0500

    UserSettings

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './Stylesheet.css'
 // import Profile from "./Components/UserProfile"
 // import Signup from "./Components/Sign-up"
 // import Verify from "./Components/Verification"
-import Report from "./Components/Reports"
+// import Report from "./Components/Reports"
+import Settings from "./Components/UserSettings" 
 
 function App() {
   return (
     <div className="div">
-      <Report />
+      <Settings />
     </div>
   );
 }
